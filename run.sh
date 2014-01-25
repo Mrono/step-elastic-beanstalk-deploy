f6f0a8d72b9973160bba637043988a2f14beef03
Michael James <mjames@raceentry.com>
2014-01-24 19:20:52 -0700

    Forgot to add step name to env vars

--- run.sh
+++ run.sh
@@ -2,19 +2,19 @@
 set +e
 
 cd $HOME
-if [ ! -n "$WERCKER_AWS_APP_NAME" ]
+if [ ! -n "$WERCKER_ELASTIC_BEANSTALK_DEPLOY_APP_NAME" ]
 then
     fail 'Missing or empty option AWS_APP_NAME, please check wercker.yml'
 fi
-if [ ! -n "$WERCKER_AWS_ENV_NAME" ]
+if [ ! -n "$WERCKER_ELASTIC_BEANSTALK_DEPLOY_ENV_NAME" ]
 then
     fail 'Missing or empty option AWS_ENV_NAME, please check wercker.yml'
 fi
-if [ ! -n "$WERCKER_AWS_KEY" ]
+if [ ! -n "$WERCKER_ELASTIC_BEANSTALK_DEPLOY_KEY" ]
 then
     fail 'Missing or empty option AWS_KEY, please check wercker.yml'
 fi
-if [ ! -n "$WERCKER_AWS_SECRET_KEY" ]
+if [ ! -n "$WERCKER_ELASTIC_BEANSTALK_DEPLOY_SECRET_KEY" ]
 then
     fail 'Missing or empty option AWS_SECRET_KEY, please check wercker.yml'
 fi
@@ -39,21 +39,21 @@ fi
 export AWS_CREDENTIAL_FILE="/home/ubuntu/.elasticbeanstalk/aws_credential_file"
 
 echo 'Setting up credentials'
-echo 'AWSAccessKeyId=$WERCKER_AWS_KEY' > $AWS_CREDENTIAL_FILE
-echo 'AWSSecretKey=$WERCKER_AWS_SECRET_KEY' >> $AWS_CREDENTIAL_FILE
+echo 'AWSAccessKeyId=$WERCKER_ELASTIC_BEANSTALK_DEPLOY_KEY' > $AWS_CREDENTIAL_FILE
+echo 'AWSSecretKey=$WERCKER_ELASTIC_BEANSTALK_DEPLOY_SECRET_KEY' >> $AWS_CREDENTIAL_FILE
 
 cat <<EOT >> $WERCKER_SOURCE_DIR/.elasticbeanstalk/config
 [global]
-ApplicationName=$WERCKER_AWS_APP_NAME
+ApplicationName=$WERCKER_ELASTIC_BEANSTALK_DEPLOY_APP_NAME
 DevToolsEndpoint=git.elasticbeanstalk.us-west-2.amazonaws.com
 Region=us-west-2
 ServiceEndpoint=https://elasticbeanstalk.us-west-2.amazonaws.com
-EnvironmentName=$WERCKER_AWS_ENV_NAME
+EnvironmentName=$WERCKER_ELASTIC_BEANSTALK_DEPLOY_ENV_NAME
 [branches]
-$CURRENT_BRANCH=$WERCKER_AWS_ENV_NAME
+$CURRENT_BRANCH=$WERCKER_ELASTIC_BEANSTALK_DEPLOY_ENV_NAME
 [branch:$CURRENT_BRANCH]
 ApplicationVersionName=$CURRENT_BRANCH
-EnvironmentName=$WERCKER_AWS_ENV_NAME
+EnvironmentName=$WERCKER_ELASTIC_BEANSTALK_DEPLOY_ENV_NAME
 InstanceProfileName=aws-elasticbeanstalk-ec2-role
 EOT
 
